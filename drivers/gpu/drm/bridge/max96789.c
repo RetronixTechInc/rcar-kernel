6ccd733113b06c2a1f39acac278ffdfe64912eb1
KC <kcyang@retronix.com.tw>
2024-04-17 14:36:38 +0800

    create max96789.c .h

--- drivers/gpu/drm/bridge/max96789.c
+++ drivers/gpu/drm/bridge/max96789.c
@@ -27,22 +27,240 @@
 #include <drm/drm_probe_helper.h>
 #include <drm/drm_edid.h>
 
-struct max96789_priv {
-	struct device *dev;
-	struct i2c_client *client;
-	struct regmap *regmap;
-	struct gpio_desc *gpiod_pwdn;
-	struct device_node		*host_node;
+#include "max96789.h"
 
-	struct drm_bridge bridge;
-	struct drm_bridge *next_bridge;
-};
+static unsigned int DEBUG_COLOR_PATTERN = 1;	/* 0: close, 1: MAX96789 */
+module_param_named(patgen, DEBUG_COLOR_PATTERN, int, 0644);
+
+static unsigned int PATTERN_TYPE = 1;			/* 1: gradient, 2: chessboard */
+module_param_named(patgen_type, PATTERN_TYPE, int, 0644);
 
 #define bridge_to_max96789_priv(b) \
 	container_of(b, struct max96789_priv, bridge)
 
 #define connector_to_max96789_priv(c) \
 	container_of(c, struct max96789_priv, connector);
+
+/* -----------------------------------------------------------------------------
+ * test pattern
+ */
+static int max96789_patgen(struct max96789_priv *priv, int pat_flags)
+{
+	int ret = 0;
+	u32 xres = 0, yres = 0;
+	u32 hfp = 0, hbp = 0, hsa = 0, vfp = 0, vbp = 0, vsa = 0;
+	xres = 1280;
+	yres = 768;
+	hbp	 = 6;
+	hfp	 = 200;
+	hsa	 = 14;
+	vbp	 = 20;
+	vfp	 = 20;
+	vsa	 = 16;
+
+	u32 vtotal 	= vfp + vsa + vbp + yres;
+	u32 htotal 	= xres + hfp + hbp + hsa;
+	u32 vs_high = vsa * htotal;
+	u32 vs_low 	= (vfp + yres + vbp) * htotal;
+	u32 hs_high = hsa;
+	u32 hs_low 	= xres + hfp + hbp;
+	u32 de_high = xres;
+	u32 de_low 	= hfp + hsa + hbp;
+	u32 de_cnt 	= yres;
+	u32 v2h 	= (vsa + vbp) * htotal + hfp;		/* Set HS Delay */
+	u32 v2d 	= v2h + hsa + hbp;					/* Set DE Delay */
+	
+	max96789_write_n(priv, MAX96789_VTX_X(2) , 3, 0);			/* VS delay */
+	max96789_write_n(priv, MAX96789_VTX_X(11), 3, v2h);			/* HS delay */
+	max96789_write_n(priv, MAX96789_VTX_X(20), 3, v2d);			/* DE delay */
+	max96789_write_n(priv, MAX96789_VTX_X(5) , 3, vs_high); 
+	max96789_write_n(priv, MAX96789_VTX_X(8) , 3, vs_low);
+	max96789_write_n(priv, MAX96789_VTX_X(14), 2, hs_high);
+	max96789_write_n(priv, MAX96789_VTX_X(16), 2, hs_low);	
+	max96789_write_n(priv, MAX96789_VTX_X(23), 2, de_high);		
+	max96789_write_n(priv, MAX96789_VTX_X(25), 2, de_low);		
+	max96789_write_n(priv, MAX96789_VTX_X(27), 2, de_cnt);		
+	
+	/* pclk detect, select VS trigger edge */
+	max96789_write_reg(priv, MAX96789_VTX_X(1), 0x21);
+
+	/* Generate VS, HS and DE in free-running mode. */
+	max96789_write_reg(priv, MAX96789_VTX_X(0), 0xFB);
+	
+	/* Set gradient increment for gradient pattern. */
+	max96789_write_reg(priv, MAX96789_VTX_X(30), 0x03);
+	
+	/* Set color for chessboard pattern. */
+	max96789_write_n(priv, MAX96789_VTX_X(31), 3, 0xFF0000);
+	max96789_write_n(priv, MAX96789_VTX_X(34), 3, 0x0000FF);
+	max96789_write_n(priv, MAX96789_VTX_X(37), 3, 0x505050);
+	
+	/* Select pattern : 0x10 is gradient, 0x01 is chessboard, 
+	 * 0x00 is pattern generator disabled - use video from the serializer input */
+	max96789_write_reg(priv, MAX96789_VTX_X(29), pat_flags == 1 ? 0x10 : 0x01);
+
+	return ret;
+}
+
+/* -----------------------------------------------------------------------------
+ * chip initialize
+ */
+static void max96789_preinit(struct max96789_priv *priv)
+{
+	max96789_update_bits(priv, MAX96789_CTRL0, BIT(7), BIT(7));	
+	usleep_range(10000, 20000);
+	
+	max96789_write_reg(priv, MAX96789_GPIO_A(0), 0x84);	// GPIO_RX
+	max96789_write_reg(priv, MAX96789_GPIO_C(0), 0x0B);	// GPIO_RX_ID
+	max96789_write_reg(priv, MAX96789_GPIO_A(1), 0x63);	// GPIO_TX
+	max96789_write_reg(priv, MAX96789_GPIO_B(1), 0x2C);	// GPIO_TX_ID
+	
+	max96789_write_reg(priv, MAX96789_REG5, 0x40);	// ERRB output to GPIO
+	usleep_range(10000, 20000);
+	
+	/* enable internal regulator for 1.2V VDD supply */
+	max96789_update_bits(priv, MAX96789_CTRL0, BIT(2), BIT(2));	// REG_EN
+	max96789_update_bits(priv, MAX96789_CTRL2, BIT(4), BIT(4));	// enable VDD LDO regulator
+	
+	/* I2C-to-I2C Slave Timeout Setting */
+	max96789_write_reg(priv, MAX96789_I2C_0, 0x01);
+	// Timeout = 1ms, 397Kbps - set for I2C fast or fast-mode plus speed
+	max96789_write_reg(priv, MAX96789_I2C_0, 0x51);
+	
+	// Link AB
+	max96789_update_bits(priv, MAX96789_CTRL1, 0x05, 0x05);
+	max96789_update_bits(priv, MAX96789_REG4, 0x50, 0);
+	
+}
+
+static void max96789_gmsl2_initial_setup(struct max96789_priv *priv)
+{
+	max96789_update_bits(priv, MAX96789_REG4, 0x50, 0x50);
+	max96789_write_reg(priv, MAX96789_REG1, 0x08);
+}
+
+static int max96789_mipi_setup(struct max96789_priv *priv)
+{
+	// DSI port A selection for video pipeline X
+	max96789_write_reg(priv, MAX96789_FRONTTOP_0, 0x5E);	
+	
+
+	//max96712_write_reg(priv, MAX_MIPI_PHY0, 0x04);
+	//max96712_write_reg(priv, MAX_MIPI_PHY3, 0xe4);
+	//max96712_write_reg(priv, MAX_MIPI_PHY4, 0xe4);
+
+	//if (priv->cphy_connection) 
+	//{
+		//max96712_write_reg(priv, MAX_MIPI_TX10(1), 0xa0);
+		//max96712_write_reg(priv, MAX_MIPI_TX10(2), 0xa0);
+		//max96712_write_reg(priv, MAX_MIPI_PHY13, 0x3F);
+		//max96712_write_reg(priv, MAX_MIPI_PHY14, 0x7D);
+	//} 
+	//else 
+	//{
+		//max96712_write_reg(priv, MAX_MIPI_TX10(1), 0xc0);
+		//max96712_write_reg(priv, MAX_MIPI_TX10(2), 0xc0);
+	//}
+
+	//max96712_update_bits(priv, MAX_BACKTOP22(0), 0x3f, ((MAX96712_DPLL_FREQ / 100) & 0x1f) | BIT(5));
+	//max96712_update_bits(priv, MAX_BACKTOP25(0), 0x3f, ((MAX96712_DPLL_FREQ / 100) & 0x1f) | BIT(5));
+	//max96712_update_bits(priv, MAX_BACKTOP28(0), 0x3f, ((MAX96712_DPLL_FREQ / 100) & 0x1f) | BIT(5));
+	//max96712_update_bits(priv, MAX_BACKTOP31(0), 0x3f, ((MAX96712_DPLL_FREQ / 100) & 0x1f) | BIT(5));
+
+	//max96712_update_bits(priv, MAX_MIPI_PHY2, 0xf0, 0xf0);
+	//if (priv->phy_pol_inv)
+		//max96712_write_reg(priv, MAX_MIPI_PHY5, 0x10);
+
+	//usleep_range(10000, 20000);
+
+	return 0;
+}
+
+static void max96789_pipe_override(struct max96789_priv *priv,
+								   unsigned int pipe,
+								   unsigned int dt, unsigned int vc)
+{
+	// EN virtual channel, bpp, datatype for video pipeline X
+	max96789_update_bits(priv, MAX96789_FRONTTOP_20, 0xFF, 0xA8);
+	max96789_update_bits(priv, MAX96789_FRONTTOP_25, 0x3F, dt);
+	
+	// video pipe crossbar (X->X, Y->Y, Z->Z, U->U)
+	max96789_update_bits(priv, MAX96789_FRONTTOP_30, 0xFF, 0xE4);
+}
+
+static void max96789_gmsl2_link_pipe_setup(struct max96789_priv *priv, int link_n)
+{
+	//struct max96789_link *link = priv->link[link_n];
+	int pipe = link_n; /* straight mapping */
+	int dt = priv->dt; /* must come from imager */
+	int in_vc = 0;
+	int i;
+
+	// packets are transmitted over GMSL A
+	max96789_write_reg(priv, MAX96789_VIDEOX_TX3(0), 0x10);
+
+	// start video pipe X from DSI port A
+	max96789_write_reg(priv, MAX96789_FRONTTOP_9, 0x00);
+	usleep_range(2000, 5000);
+	
+	max96789_pipe_override(priv, pipe, dt, in_vc);
+	usleep_range(2000, 5000);
+	
+
+	//max96712_write_reg(priv, MAX96712_RX0(pipe), 0);
+	//max96712_write_reg(priv, MAX_VIDEO_RX0(pipe), 0x00);
+	//max96712_pipe_override(priv, pipe, dt, in_vc);
+	//usleep_range(2000, 5000);
+
+	//max96712_write_reg(priv, MAX_MIPI_TX11(pipe), 0x00);	/* disable all mappings */
+	//max96712_write_reg(priv, MAX_MIPI_TX12(pipe), 0x00);
+	
+////printk("[gmsl2_link_pipe_setup] pipe: %d, link_n: %d, in_vc: %d\n", pipe, link_n, in_vc);
+	//for (i = 0; i < ARRAY_SIZE(gmsl2_pipe_maps); i++) 
+	//{
+		//max96712_mapping_pipe_to_mipi(priv, pipe, i,							/* pipe, map# */
+									  //gmsl2_pipe_maps[i].in_dt, in_vc,			/* src DT, VC */
+									  //gmsl2_pipe_maps[i].out_dt, link->out_vc,	/* dst DT, VC */
+									  //link->out_mipi);							/* dst MIPI PHY */
+	//}
+
+	//link->pipes_mask |= BIT(pipe);
+	
+
+}
+
+static void max96789_initialize(struct max96789_priv *priv)
+{
+	//struct max96789_source *source;
+	int ret;
+	int link = 0;
+
+	max96789_preinit(priv);
+	max96789_gmsl2_initial_setup(priv);
+	max96789_mipi_setup(priv);	// MIPI-DSI
+
+	max96789_gmsl2_link_pipe_setup(priv, 0);
+	
+	///* Start all cameras. */
+	//for_each_source(priv, source) 
+	//{
+		//max96712_gmsl2_link_pipe_setup(priv, link);
+		//ret = max96712_gmsl2_reverse_channel_setup(priv, link);
+		//if (ret < 0)
+			//source->linkup = false;
+		//else
+			//source->linkup = true;
+		//link++;
+	//}
+	//max96712_gmsl2_fsync_setup(priv);
+	
+	if (DEBUG_COLOR_PATTERN == 1)
+	{
+		printk("[max96789_patgen]: %d\n", __LINE__);
+		max96789_patgen(priv, PATTERN_TYPE);
+	}
+	return;
+}
 
 /* -----------------------------------------------------------------------------
  * DRM Bridge Operations
@@ -60,13 +278,15 @@ static int max96789_bridge_attach(struct drm_bridge *bridge,
 						 };
 
 	host = of_find_mipi_dsi_host_by_node(priv->host_node);
-	if (!host) {
+	if (!host) 
+	{
 		DRM_ERROR("failed to find dsi host\n");
 		return -ENODEV;
 	}
 
 	dsi = mipi_dsi_device_register_full(host, &info);
-	if (IS_ERR(dsi)) {
+	if (IS_ERR(dsi)) 
+	{
 		DRM_ERROR("failed to create dsi device\n");
 		return PTR_ERR(dsi);
 	}
@@ -76,14 +296,14 @@ static int max96789_bridge_attach(struct drm_bridge *bridge,
 	dsi->mode_flags = MIPI_DSI_MODE_VIDEO;
 
 	ret = mipi_dsi_attach(dsi);
-	if (ret < 0) {
+	if (ret < 0) 
+	{
 		DRM_ERROR("failed to attach dsi to host\n");
 		mipi_dsi_device_unregister(dsi);
 		return ret;
 	}
-
-	return drm_bridge_attach(bridge->encoder, priv->next_bridge, bridge,
-			flags);
+printk("%s (line %d) ret : %d\n", __FUNCTION__, __LINE__, ret);
+	return drm_bridge_attach(bridge->encoder, priv->next_bridge, bridge, flags);
 }
 
 static void max96789_bridge_enable(struct drm_bridge *bridge)
@@ -91,6 +311,8 @@ static void max96789_bridge_enable(struct drm_bridge *bridge)
 	struct max96789_priv *priv = bridge_to_max96789_priv(bridge);
 
 	gpiod_set_value_cansleep(priv->gpiod_pwdn, 1);
+	
+	max96789_initialize(priv);
 }
 
 static void max96789_bridge_disable(struct drm_bridge *bridge)
@@ -139,11 +361,19 @@ static int max96789_bridge_probe(struct i2c_client *client)
 	if (priv->gpiod_pwdn)
 		usleep_range(4000, 5000);
 
+	priv->client = client;
+	priv->dt = MIPI_DT_RGB888;
+	i2c_set_clientdata(client, priv);
+	
+	max96789_initialize(priv);
+
 	ret = drm_of_find_panel_or_bridge(priv->dev->of_node, 1, 0,
 				NULL, &priv->next_bridge);
-	if (ret) {
+				
+	if (ret) 
+	{
 		DRM_ERROR("could not find bridge node\n");
-		return ret;
+		//return ret;
 	}
 
 	priv->host_node = of_graph_get_remote_node(priv->dev->of_node
---
+++ drivers/gpu/drm/bridge/max96789.h
@@ -0,0 +1,546 @@
+
+#define MAX96789_PWR4  				0X0C
+#define MAX96789_REG1				0x01
+#define MAX96789_REG2				0x02
+#define MAX96789_REG4				0x04
+#define MAX96789_REG5				0x05
+
+#define MAX96789_CTRL0 				0X10
+#define MAX96789_CTRL1 				0X11
+#define MAX96789_CTRL2				0X12
+
+#define MAX96789_GPIO_BASE(n)		(0x2BE + n)
+#define MAX96789_GPIO_A(n) 			(MAX96789_GPIO_BASE(0) + (3 * n))
+#define MAX96789_GPIO_B(n) 			(MAX96789_GPIO_BASE(1) + (3 * n))
+#define MAX96789_GPIO_C(n) 			(MAX96789_GPIO_BASE(2) + (3 * n))
+
+#define MAX96789_I2C_0				0x40
+#define MAX96789_I2C_1				0x41
+
+#define MAX96789_FRONTTOP_0			0x308
+#define MAX96789_FRONTTOP_9			0x311
+#define MAX96789_FRONTTOP_20		0x31C
+#define MAX96789_FRONTTOP_25		0x321
+#define MAX96789_FRONTTOP_30		0x326
+
+#define MAX96789_VIDEO_TX_BASE(n)	(0x50 + n * 4)
+#define MAX96789_VIDEOX_TX0(n)		(MAX96789_VIDEO_TX_BASE(n) + 0)
+#define MAX96789_VIDEOX_TX1(n)		(MAX96789_VIDEO_TX_BASE(n) + 1)
+#define MAX96789_VIDEOX_TX3(n)		(MAX96789_VIDEO_TX_BASE(n) + 3)
+
+#define MAX96789_VTX_BASE(n)		(0x1B0 + 0x43 * n)
+#define MAX96789_VTX_X(n)			(MAX96789_VTX_BASE(0) + 0x18 + n)
+#define MAX96789_VTX_Y(n)			(MAX96789_VTX_BASE(1) + 0x18 + n)
+#define MAX96789_VTX_Z(n)			(MAX96789_VTX_BASE(2) + 0x18 + n)
+#define MAX96789_VTX_U(n)			(MAX96789_VTX_BASE(3) + 0x18 + n)
+
+#define MIPI_DT_RGB888			0x24
+
+struct max96789_priv {
+	struct device *dev;
+	struct i2c_client *client;
+	struct regmap *regmap;
+	struct gpio_desc *gpiod_pwdn;
+	struct device_node		*host_node;
+
+	struct drm_bridge bridge;
+	struct drm_bridge *next_bridge;
+	
+	int links_mask;
+	int dt;
+};
+
+struct max96789_source {
+	struct fwnode_handle *fwnode;
+	bool linkup;
+};
+
+struct max96789_link {
+	struct fwnode_handle *sd_fwnode;
+	struct i2c_client *client;
+	int des_id;
+	int des_addr;
+	int pipes_mask;			/* mask of pipes used by this link */
+	int out_mipi;			/* MIPI# */
+	int out_vc;				/* VC# */
+	struct regulator *poc_reg;	/* PoC power supply */
+};
+
+/* -----------------------------------------------------------------------------
+ * I2C IO
+ */
+static int max96789_write_reg(struct max96789_priv *priv, u16 reg, u8 val)
+{
+	int ret;
+	
+	u8 regbuf[3];
+	regbuf[0] = reg >> 8;
+	regbuf[1] = reg & 0xff;
+	regbuf[2] = val;
+	//regmap_raw_write(link->regmap, reg, &val, 3);
+	ret = i2c_master_send(priv->client, regbuf, 3);
+	msleep(5);
+	if (ret < 0) 
+	{
+		dev_err(&priv->client->dev, "%s: write reg error %d: reg=%x, val=%x\n", __func__, ret, reg, val);
+		return ret;
+	}
+	return 0;
+}
+
+
+static int max96789_write_n(struct max96789_priv *priv, int reg, int val_count, int val)
+{
+	int ret;
+	int i;
+	
+	u8 values[3];
+	for (i = 0; i < val_count; i++)
+	{
+		values[i] = (val >> ((val_count - i - 1) * 8)) & 0xff;
+		ret = max96789_write_reg(priv, reg, values[i]);
+		if (ret)
+		{
+			dev_dbg(&priv->client->dev, "write register 0x%04x failed (%d)\n", reg, ret);
+			return ret;
+		}
+		reg += 1;
+	}
+	return 0;
+}
+
+static int max96789_read(struct max96789_priv *priv, u16 reg, u8 *val)
+{
+	int ret;
+	u8 regbuf[2];
+	regbuf[0] = reg >> 8;
+	regbuf[1] = reg & 0xff;
+
+	ret = i2c_master_send(priv->client, regbuf, 2);
+	if (ret < 0) 
+	{
+		dev_err(&priv->client->dev, "%s: write reg error %d: reg=%x\n", __func__, ret, reg);
+		return ret;
+	}
+
+	ret = i2c_master_recv(priv->client, val, 1);
+	if (ret < 0) 
+	{
+		dev_err(&priv->client->dev, "%s: read reg error %d: reg=%x\n", __func__, ret, reg);
+		return ret;
+	}
+	return 0;
+}
+
+static inline int max96789_update_bits(struct max96789_priv *priv, u16 reg,
+				       u8 mask, u8 bits)
+{
+	int ret, tmp;
+	u8 val;
+
+	ret = max96789_read(priv, reg, &val);
+	if (ret != 0)
+		return ret;
+
+	tmp = val & ~mask;
+	tmp |= bits & mask;
+
+	if (tmp != val)
+		ret = max96789_write_reg(priv, reg, tmp);
+	return ret;
+}
+
+//------------------------------------------
+
+//static inline char *chip_name(int id)
+//{
+	//switch (id) {
+	//case MAX9271_ID:
+		//return "MAX9271";
+	//case MAX9286_ID:
+		//return "MAX9286";
+	//case MAX9288_ID:
+		//return "MAX9288";
+	//case MAX9290_ID:
+		//return "MAX9290";
+	//case MAX9295A_ID:
+		//return "MAX9295A";
+	//case MAX9295B_ID:
+		//return "MAX9295B";
+	//case MAX9296A_ID:
+		//return "MAX9296A";
+	//case MAX96705_ID:
+		//return "MAX96705";
+	//case MAX96706_ID:
+		//return "MAX96706";
+	//case MAX96707_ID:
+		//return "MAX96707";
+	//case MAX96712_ID:
+		//return "MAX96712";
+	//default:
+		//return "serializer";
+	//}
+//}
+
+//static inline int reg8_read(struct i2c_client *client, u8 reg, u8 *val)
+//{
+	//int ret, retries;
+
+	//for (retries = REG8_NUM_RETRIES; retries; retries--) {
+		//ret = i2c_smbus_read_byte_data(client, reg);
+		//if (!(ret < 0))
+			//break;
+	//}
+
+	//if (ret < 0) {
+		//dev_dbg(&client->dev,
+			//"read fail: chip 0x%x register 0x%x: %d\n",
+			//client->addr, reg, ret);
+	//} else {
+		//*val = ret;
+	//}
+
+	//return ret < 0 ? ret : 0;
+//}
+
+//static inline int reg8_write(struct i2c_client *client, u8 reg, u8 val)
+//{
+	//int ret, retries;
+
+	//for (retries = REG8_NUM_RETRIES; retries; retries--) {
+		//ret = i2c_smbus_write_byte_data(client, reg, val);
+		//if (!(ret < 0))
+			//break;
+	//}
+
+	//if (ret < 0) {
+		//dev_dbg(&client->dev,
+			//"write fail: chip 0x%x register 0x%x: %d\n",
+			//client->addr, reg, ret);
+	//} else {
+//#ifdef WRITE_VERIFY
+		//u8 val2;
+		//reg8_read(client, reg, &val2);
+		//if (val != val2)
+			//dev_err(&client->dev,
+				//"write verify mismatch: chip 0x%x reg=0x%x "
+				//"0x%x->0x%x\n", client->addr, reg, val, val2);
+//#endif
+	//}
+
+	//return ret < 0 ? ret : 0;
+//}
+
+//static inline int reg16_read(struct i2c_client *client, u16 reg, u8 *val)
+//{
+	//int ret, retries;
+	//u8 buf[2] = {reg >> 8, reg & 0xff};
+
+	//for (retries = REG16_NUM_RETRIES; retries; retries--) {
+		//ret = i2c_master_send(client, buf, 2);
+		//if (ret == 2) {
+			//ret = i2c_master_recv(client, buf, 1);
+			//if (ret == 1)
+				//break;
+		//}
+	//}
+
+	//if (ret < 0) {
+		//dev_dbg(&client->dev,
+			//"read fail: chip 0x%x register 0x%x: %d\n",
+			//client->addr, reg, ret);
+	//} else {
+		//*val = buf[0];
+	//}
+
+	//return ret < 0 ? ret : 0;
+//}
+
+//static inline int reg16_write(struct i2c_client *client, u16 reg, u8 val)
+//{
+	//int ret, retries;
+	//u8 buf[3] = {reg >> 8, reg & 0xff, val};
+
+	//for (retries = REG16_NUM_RETRIES; retries; retries--) {
+		//ret = i2c_master_send(client, buf, 3);
+		//if (ret == 3)
+			//break;
+	//}
+
+	//if (ret < 0) {
+		//dev_dbg(&client->dev,
+			//"write fail: chip 0x%x register 0x%x: %d\n",
+			//client->addr, reg, ret);
+	//} else {
+//#ifdef WRITE_VERIFY
+		//u8 val2;
+		//reg16_read(client, reg, &val2);
+		//if (val != val2)
+			//dev_err(&client->dev,
+				//"write verify mismatch: chip 0x%x reg=0x%x "
+				//"0x%x->0x%x\n", client->addr, reg, val, val2);
+//#endif
+	//}
+
+	//return ret < 0 ? ret : 0;
+//}
+
+//static inline int reg16_read16(struct i2c_client *client, u16 reg, u16 *val)
+//{
+	//int ret, retries;
+	//u8 buf[2] = {reg >> 8, reg & 0xff};
+
+	//for (retries = REG8_NUM_RETRIES; retries; retries--) {
+		//ret = i2c_master_send(client, buf, 2);
+		//if (ret == 2) {
+			//ret = i2c_master_recv(client, buf, 2);
+			//if (ret == 2)
+				//break;
+		//}
+	//}
+
+	//if (ret < 0) {
+		//dev_dbg(&client->dev,
+			//"read fail: chip 0x%x register 0x%x: %d\n",
+			//client->addr, reg, ret);
+	//} else {
+		//*val = ((u16)buf[0] << 8) | buf[1];
+	//}
+
+	//return ret < 0 ? ret : 0;
+//}
+
+//static inline int reg16_write16(struct i2c_client *client, u16 reg, u16 val)
+//{
+	//int ret, retries;
+	//u8 buf[4] = {reg >> 8, reg & 0xff, val >> 8, val & 0xff};
+
+	//for (retries = REG8_NUM_RETRIES; retries; retries--) {
+		//ret = i2c_master_send(client, buf, 4);
+		//if (ret == 4)
+			//break;
+	//}
+
+	//if (ret < 0) {
+		//dev_dbg(&client->dev,
+			//"write fail: chip 0x%x register 0x%x: %d\n",
+			//client->addr, reg, ret);
+	//}
+
+	//return ret < 0 ? ret : 0;
+//}
+
+//static inline int reg16_read_n(struct i2c_client *client,
+			       //u16 reg, u8 *val, int n)
+//{
+	//int ret, retries;
+	//u8 buf[2] = {reg >> 8, reg & 0xff};
+
+	//for (retries = REG16_NUM_RETRIES; retries; retries--) {
+		//ret = i2c_master_send(client, buf, 2);
+		//if (ret == 2) {
+			//ret = i2c_master_recv(client, val, n);
+			//if (ret == n)
+				//break;
+		//}
+	//}
+
+	//if (ret < 0) {
+		//dev_dbg(&client->dev,
+			//"read fail: chip 0x%x registers 0x%x-0x%x: %d\n",
+			//client->addr, reg, reg + n, ret);
+	//}
+
+	//return ret < 0 ? ret : 0;
+//}
+
+//static inline int reg8_read_addr(struct i2c_client *client, int addr,
+				 //u8 reg, u8 *val)
+//{
+	//int ret, retries;
+	//union i2c_smbus_data data;
+
+	//for (retries = REG8_NUM_RETRIES; retries; retries--) {
+		//ret = i2c_smbus_xfer(client->adapter, addr, client->flags,
+				     //I2C_SMBUS_READ, reg,
+				     //I2C_SMBUS_BYTE_DATA, &data);
+		//if (!(ret < 0))
+			//break;
+	//}
+
+	//if (ret < 0) {
+		//dev_dbg(&client->dev,
+			//"read fail: chip 0x%x register 0x%x: %d\n",
+			//addr, reg, ret);
+	//} else {
+		//*val = data.byte;
+	//}
+
+	//return ret < 0 ? ret : 0;
+//}
+
+//static inline int reg8_write_addr(struct i2c_client *client, u8 addr,
+				  //u8 reg, u8 val)
+//{
+	//int ret, retries;
+	//union i2c_smbus_data data;
+
+	//data.byte = val;
+
+	//for (retries = REG8_NUM_RETRIES; retries; retries--) {
+		//ret = i2c_smbus_xfer(client->adapter, addr, client->flags,
+				     //I2C_SMBUS_WRITE, reg,
+				     //I2C_SMBUS_BYTE_DATA, &data);
+		//if (!(ret < 0))
+			//break;
+	//}
+
+	//if (ret < 0) {
+		//dev_dbg(&client->dev,
+			//"write fail: chip 0x%x register 0x%x value 0x%0x: %d\n",
+			//addr, reg, val, ret);
+	//}
+
+	//return ret < 0 ? ret : 0;
+//}
+
+
+//static inline int reg16_write_addr(struct i2c_client *client, int chip,
+				   //u16 reg, u8 val)
+//{
+	//struct i2c_msg msg[1];
+	//u8 wbuf[3];
+	//int ret;
+
+	//msg->addr = chip;
+	//msg->flags = 0;
+	//msg->len = 3;
+	//msg->buf = wbuf;
+	//wbuf[0] = reg >> 8;
+	//wbuf[1] = reg & 0xff;
+	//wbuf[2] = val;
+
+	//ret = i2c_transfer(client->adapter, msg, 1);
+	//if (ret < 0) {
+		//dev_dbg(&client->dev,
+			//"i2c fail: chip 0x%02x wr 0x%04x (0x%02x): %d\n",
+			//chip, reg, val, ret);
+		//return ret;
+	//}
+
+	//return 0;
+//}
+
+//static inline int reg16_read_addr(struct i2c_client *client, int chip,
+				  //u16 reg, int *val)
+//{
+	//struct i2c_msg msg[2];
+	//u8 wbuf[2];
+	//u8 rbuf[1];
+	//int ret;
+
+	//msg[0].addr = chip;
+	//msg[0].flags = 0;
+	//msg[0].len = 2;
+	//msg[0].buf = wbuf;
+	//wbuf[0] = reg >> 8;
+	//wbuf[1] = reg & 0xff;
+
+	//msg[1].addr = chip;
+	//msg[1].flags = I2C_M_RD;
+	//msg[1].len = 1;
+	//msg[1].buf = rbuf;
+
+	//ret = i2c_transfer(client->adapter, msg, 2);
+	//if (ret < 0) {
+		//dev_dbg(&client->dev, "i2c fail: chip 0x%02x rd 0x%04x: %d\n",
+			//chip, reg, ret);
+		//return ret;
+	//}
+
+	//*val = rbuf[0];
+
+	//return 0;
+//}
+
+//#define __reg8_read(addr, reg, val)	reg8_read_addr(priv->client, addr, reg, val)
+//#define __reg8_write(addr, reg, val)	reg8_write_addr(priv->client, addr, reg, val)
+//#define __reg16_read(addr, reg, val)	reg16_read_addr(priv->client, addr, reg, val)
+//#define __reg16_write(addr, reg, val)	reg16_write_addr(priv->client, addr, reg, val)
+
+//struct i2c_mux_priv {
+	//struct i2c_adapter adap;
+	//struct i2c_algorithm algo;
+	//struct i2c_mux_core *muxc;
+	//u32 chan_id;
+//};
+
+//static inline int get_des_id(struct i2c_client *client)
+//{
+	//struct i2c_mux_priv *mux_priv = client->adapter->algo_data;
+
+	//if (!strcmp(mux_priv->muxc->dev->driver->name, "max9286"))
+		//return MAX9286_ID;
+	//if (!strcmp(mux_priv->muxc->dev->driver->name, "max9288"))
+		//return MAX9288_ID;
+	//if (!strcmp(mux_priv->muxc->dev->driver->name, "max9296"))
+		//return MAX9296A_ID;
+	//if (!strcmp(mux_priv->muxc->dev->driver->name, "max96706"))
+		//return MAX96706_ID;
+	//if (!strcmp(mux_priv->muxc->dev->driver->name, "max96712"))
+		//return MAX96712_ID;
+	//if (!strcmp(mux_priv->muxc->dev->driver->name, "ti9x4"))
+		//return UB960_ID;
+
+	//return -EINVAL;
+//}
+
+//static inline int get_des_addr(struct i2c_client *client)
+//{
+	//struct i2c_mux_priv *mux_priv = client->adapter->algo_data;
+
+	//return to_i2c_client(mux_priv->muxc->dev)->addr;
+//}
+
+//static inline void setup_i2c_translator(struct i2c_client *client, int ser_addr,
+					//int sensor_addr)
+//{
+	//int gmsl_mode = MODE_GMSL2;
+
+	//switch (get_des_id(client)) {
+	//case MAX9286_ID:
+	//case MAX9288_ID:
+	//case MAX96706_ID:
+		//reg8_write_addr(client, ser_addr, 0x09, client->addr << 1);
+		//reg8_write_addr(client, ser_addr, 0x0A, sensor_addr << 1);
+		//break;
+	//case MAX9296A_ID:
+	//case MAX96712_ID:
+		///* parse gmsl mode from deserializer */
+		//reg16_read_addr(client, get_des_addr(client), 6, &gmsl_mode);
+		//gmsl_mode = !!(gmsl_mode & BIT(7)) + 1;
+
+		//if (gmsl_mode == MODE_GMSL1) {
+			//reg8_write_addr(client, ser_addr, 0x09,
+					//client->addr << 1);
+			//reg8_write_addr(client, ser_addr, 0x0A,
+					//sensor_addr << 1);
+		//}
+		//if (gmsl_mode == MODE_GMSL2) {
+			//reg16_write_addr(client, ser_addr, MAX9295_I2C2,
+					 //client->addr << 1);
+			//reg16_write_addr(client, ser_addr, MAX9295_I2C3,
+					 //sensor_addr << 1);
+		//}
+		//break;
+	//case UB960_ID:
+		//reg8_write_addr(client, get_des_addr(client), 0x65,
+				//client->addr << 1);
+		//reg8_write_addr(client, get_des_addr(client), 0x5d,
+				//sensor_addr << 1);
+		//break;
+	//}
+	//usleep_range(2000, 2500);
+//}
+
+
